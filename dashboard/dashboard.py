904dd163f0ffa8a1a636d0461f3efb47f6f9e268
P.Hrishikesh Reddy <hrishi@PHrishikeshs-MacBook-Pro.local>
2024-05-14 16:09:54 +0530

    Updated V2

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,12 +20,12 @@ spacex_df = pd.read_csv("spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
-#print(spacex_df['Launch Site'].unique())
+#print(spacex_df['Launch Site'].unique()) 
 
-# Create a dash application
+
 app = dash.Dash(__name__)
 
-# Create an app layout
+# App layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
@@ -60,8 +60,8 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
                                 ])
 
-# TASK 2:
-# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+
+# Callback function for `site-dropdown` as input, `success-pie-chart` as output
 # Function decorator to specify function input and output
 @app.callback(Output(component_id='success-pie-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'))
@@ -81,8 +81,9 @@ def get_pie_chart(entered_site):
         # return the outcomes piechart for a selected site
     return fig
         
-# TASK 4:
-# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+
+
+# Callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
               Input(component_id='site-dropdown', component_property='value'), 
               Input(component_id="payload-slider", component_property="value"))
@@ -99,6 +100,7 @@ def get_scatter_chart(entered_site, payload_range):
     
     return fig
 
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
@@ -108,6 +110,6 @@ if __name__ == '__main__':
 
 '''
 Created by Hrishikesh Reddy Papasani 
-Github link: https://github.com/Hrishikesh-Papasani
-Gmail: hrpapasani@gmail.com
+Github: https://github.com/Hrishikesh-Papasani
+Contact: hrpapasani@gmail.com
 '''
